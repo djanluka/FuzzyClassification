573e548e6b89d6e3b52db8065a8083fa8606e25d
JovanS98 <stamenkovicjovan15@gmail.com>
2020-12-23 16:14:18 +0100

    Implemented fuzzy logic

---
+++ Fuzzy.py
@@ -0,0 +1,74 @@
+class MFInput:
+
+    def __init__(self, name, x, y, x0):
+        self.name = name
+        self.points = [(x[i], y[i]) for i in range(len(x))]
+        self.mi = self.getMi(x0)
+
+    def getY(self, x1, y1, x2, y2, x0):
+
+        if y1 == y2:
+            return y1
+        if y1 < y2:
+            return (x0 - x1) / (x2 - x1)
+        return (x2 - x0) / (x2 - x1)
+
+    def getMi(self, x0):
+
+        if x0 < self.points[0][0]:
+            return self.points[0][1]
+        if x0 > self.points[-1][0]:
+            return self.points[-1][1]
+        for i in range(1, len(self.points)):
+            x1 = self.points[i - 1][0]
+            x2 = self.points[i][0]
+            if x1 <= x0 < x2:
+                y1 = self.points[i - 1][1]
+                y2 = self.points[i][1]
+                return self.getY(x1, y1, x2, y2, x0)
+        return -1
+
+
+class MFOutput:
+
+    def __init__(self, name, x, y):
+        self.name = name
+        sumX = 0
+        nb1 = 0
+        self.points = []
+        for i in range(len(x)):
+            self.points.append((x[i], y[i]))
+            if y[i] == 1:
+                sumX += x[i]
+                nb1 += 1
+        self.mi = 0
+        self.vaue = sumX / nb1
+
+
+from enum import Enum, unique
+@unique
+class Logic(Enum):
+    OR = 0
+    AND = 1
+
+class Rule:
+    def __init__(self, input1, input2, input3, input4, input5, input6, output, logic):
+        self.inputArray = []
+        self.inputArray.append(input1)
+        self.inputArray.append(input2)
+        self.inputArray.append(input3)
+        self.inputArray.append(input4)
+        self.inputArray.append(input5)
+        self.inputArray.append(input6)
+        if logic == Logic.OR:
+            maxI = input1.mi
+            for i in range (1,6):
+                maxI = max(maxI, self.InputArray[i].mi)
+            self.mfOutput.mi = max(self.mfOutput.mi, maxI)
+        else:
+            minI = input1.mi
+            for i in range(1, 6):
+                minI = min(minI, self.InputArray[i].mi)
+            self.mfOutput.mi = max(self.mfOutput.mi, minI)
+
+
